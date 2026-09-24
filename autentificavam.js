e1fc447d0e470377fc82ef7166d46cd64036659e
Gm-bug <gm5378167@gmail.com>
2026-09-24 15:57:40 -0300

    Add user authentication logic in autentificavam.js

---
+++ autentificavam.js
@@ -0,0 +1,13 @@
+const senhaUsu = '12345'
+const nomeUsu = '12345@sp'
+
+//estrutura de descisao
+if (nomeUsu === '12345@sp' && senhaUsu === '12345') {
+    alert("bem vindo ao sistema");
+    console.log('bem vindo ao sistema');
+
+}
+else {
+    alert("usuario invalido");
+    console.log("usuario invalido");
+}
