04ca692220a31843af8ca09157bcdbf62489ff7e
Gm-bug <gm5378167@gmail.com>
2026-09-24 17:24:55 -0300

    Add switch statement to handle multiple cases

---
+++ switch.js
@@ -0,0 +1,15 @@
+const chave = 1
+let resp = null
+switch (chave) {
+    case 1: resp = 'magic'
+        break
+    case 2: resp = 'banana'
+        break
+    case 3: resp = 'bicicleta'
+        break
+    case 4: res = 'morango'
+        break
+    default: resp = 'voce nao escolheu nada'
+       break
+}
+console.log(`voce escolheu ${resp}`)
